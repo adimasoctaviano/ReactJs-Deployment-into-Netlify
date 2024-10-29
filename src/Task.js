03f0fa3f216c79abeeebf457d67e649fa2b435dc
Dimas Oktavian Prasetyo <dimasoktavianprasetyo@gmail.com>
2024-10-29 13:24:00 +0700

    pra-deployment into netlify!

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL" // TODO: replace this
+const studentName = "Dimas Oktafian Prasetyo" // TODO: replace this
+const studentId = "FS9966251" // TODO: replace this
+
+export { url, studentName, studentId }
